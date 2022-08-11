240d6051e2d8123d4229fb49b13d01be60be747c
oni <rainbow2774@naver.com>
2022-08-11 23:39:34 +0900

    request 스코프 예제 이해, 발생하는 문제를 provider로 해결

---
+++ core/src/main/java/hello/core/common/MyLogger.java
@@ -0,0 +1,37 @@
+package hello.core.common;
+
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+@Component
+@Scope(value = "request")
+public class MyLogger {
+
+    private String uuid;
+    private String requestURL;
+
+    public void setRequestURL(String requestURL) {
+        this.requestURL = requestURL;
+    }
+
+    public void log(String message) {
+        System.out.println("[" + uuid + " ] " + "[" + requestURL + " ] " + message);
+    }
+
+    @PostConstruct
+    public void init() {
+        uuid = java.util.UUID.randomUUID().toString(); // 절대 겹치지 않는 Unique Id가 만들어진다.
+        System.out.println("[" + uuid + " ] request scope bean create: " + this );
+    }
+
+    @PreDestroy
+    public void close() {
+        System.out.println("[" + uuid + " ] request scope bean close: " + this );
+    }
+
+
+
+}
