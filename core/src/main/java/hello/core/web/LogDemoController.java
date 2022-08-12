0ccb68bb10ff111b57478498676471c0947a9e2a
oni <rainbow2774@naver.com>
2022-08-12 10:58:33 +0900

    provider를 안쓰고 지연처리하는 프록시모드 설정방법 이해

--- core/src/main/java/hello/core/web/LogDemoController.java
+++ core/src/main/java/hello/core/web/LogDemoController.java
@@ -14,14 +14,17 @@ import javax.servlet.http.HttpServletRequest;
 public class LogDemoController {
 
     private final LogDemoService logDemoService;
-    private final ObjectProvider<MyLogger> myLoggerProvider; // look up 할 수 있는애가 주입된다.
+//    private final ObjectProvider<MyLogger> myLoggerProvider; // look up 할 수 있는애가 주입된다.
+    private final MyLogger myLogger;
 
     @RequestMapping("log-demo")
     @ResponseBody
     public String logDemo(HttpServletRequest request) throws InterruptedException {
         String requestURL = request.getRequestURL().toString();
-        MyLogger myLogger = myLoggerProvider.getObject(); // 이 시점에 최초로 만들어 진다.
+//        MyLogger myLogger = myLoggerProvider.getObject(); // 이 시점에 최초로 만들어 진다.
+        System.out.println(myLogger.getClass());
         myLogger.setRequestURL(requestURL);
+
 
         myLogger.log("controller test");
         Thread.sleep(1000);
